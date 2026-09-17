51fd981e800534223a99aa0c445fd474b9a047b9
Yussef Zahrani <yusse@Yussefs-MacBook-Air.local>
2026-09-17 17:21:58 -0400

    Base64 Encoder/Decoder

---
+++ src/base64_coder.zig
@@ -0,0 +1,161 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        var i: u8 = 0;
+        var output_index: u8 = 0;
+
+        while (i < 64) : (i += 1) {
+            if (self._char_at(i) == char)
+                break;
+            output_index += 1;
+        }
+
+        return output_index;
+    }
+
+    fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+
+        const n_out: usize = try _calc_encode_length(input);
+        var output = try allocator.alloc(u8, n_out);
+        var buf = [3]u8{ 0, 0, 0 };
+        var count: u8 = 0;
+        var iout: u64 = 0;
+
+        for (input, 0..) |_, i| {
+            buf[count] = input[i];
+            count += 1;
+
+            if (count == 3) {
+                output[iout] = self._char_at(buf[0] >> 2);
+                output[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+                output[iout + 2] = self._char_at(((buf[1] & 0x0f) << 2) + (buf[2] >> 6));
+                output[iout + 3] = self._char_at(buf[2] & 0x03f);
+                iout += 4;
+                count = 0;
+            }
+        }
+
+        if (count == 1) {
+            output[iout] = self._char_at(buf[0] >> 2);
+            output[iout + 1] = self._char_at((buf[0] & 0x03) << 4);
+            output[iout + 2] = '=';
+            output[iout + 3] = '=';
+        }
+
+        if (count == 2) {
+            output[iout] = self._char_at(buf[0] >> 2);
+            output[iout + 1] = self._char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+            output[iout + 2] = self._char_at((buf[1] & 0x0f) << 2);
+            output[iout + 3] = '=';
+            iout += 4;
+        }
+
+        return output;
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0)
+            return "";
+
+        const n_output = try _calc_decode_length(input);
+        var tmp_buffer = [4]u8{ 0, 0, 0, 0 };
+        var count: u8 = 0;
+        var output_index: u64 = 0;
+        const output = try allocator.alloc(u8, n_output);
+        @memset(output[0..], 0);
+
+        for (0..input.len) |i| {
+            tmp_buffer[count] = self._char_index(input[i]);
+            count += 1;
+            if (count == 4) {
+                output[output_index] = (tmp_buffer[0] << 2) + (tmp_buffer[1] >> 4);
+                if (tmp_buffer[2] != 64) {
+                    output[output_index + 1] = (tmp_buffer[1] << 4) + (tmp_buffer[2] >> 2);
+                }
+                if (tmp_buffer[3] != 64) {
+                    output[output_index + 2] = (tmp_buffer[2] << 6) + tmp_buffer[3];
+                }
+                output_index += 3;
+                count = 0;
+            }
+        }
+
+        return output;
+    }
+};
+
+fn _calc_encode_length(input: []const u8) !usize {
+    if (input.len < 3) {
+        return 4;
+    }
+
+    const n_groups: usize = try std.math.divCeil(usize, input.len, 3);
+    return n_groups * 4;
+}
+
+fn _calc_decode_length(input: []const u8) !usize {
+    if (input.len < 4) {
+        return 3;
+    }
+
+    const n_groups: usize = try std.math.divFloor(usize, input.len, 4);
+
+    var multiple_groups: usize = n_groups * 3;
+    var i: usize = input.len - 1;
+
+    while (i > 0) : (i -= 1) {
+        if (input[i] == '=') {
+            multiple_groups -= 1;
+        } else {
+            break;
+        }
+    }
+
+    return multiple_groups;
+}
+
+pub fn main(init: std.process.Init) !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+
+    const text = "Testing some more stuff";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
+    const base64 = Base64.init();
+    const encoded_text = try base64.encode(allocator, text);
+    const decoded_text = try base64.decode(allocator, etext);
+
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
+    try stdout.print("Decoded text: {s}\n", .{decoded_text});
+    try stdout.print("Encoded length: {d}\n", .{encoded_text.len});
+    try stdout.print("Decoded length: {d}\n", .{decoded_text.len});
+    try stdout.flush();
+}
